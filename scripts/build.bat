cbb65d8d3d588869f0fb021f265a5e0a4d7dca6a
RGreenWolf <57684328+RGreenWolf@users.noreply.github.com>
2025-03-15 01:24:10 +0100

    Update build.bat

--- scripts/build.bat
+++ scripts/build.bat
@@ -4,7 +4,7 @@ set "current_dir=%~dp0../"
 set "src_path=%current_dir%src/main.py"
 set "assets_path=%current_dir%assets"
 set "config_path=%current_dir%config"
-set "pulic_server=%current_dir%src/server/public"
+set "pulic_server=%current_dir%src/server/routes/web/static"
 
 call "%current_dir%venv/Scripts/activate.bat"
 pyinstaller --add-data "%config_path%:config" --add-data "%pulic_server%:server_public_files" --add-data "%assets_path%:assets" --onefile "%src_path%" --icon="%assets_path%\logo\round.ico"
