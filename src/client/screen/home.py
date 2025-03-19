f5e38dd412a927ca5eed7e4f218a6e463ab7e1a6
tEtEmElE <theophile.melerowicz@gmail.com>
2025-03-19 10:15:24 +0100

    debug

--- src/client/screen/home.py
+++ src/client/screen/home.py
@@ -5,12 +5,9 @@ from client.composants import NavBar
 from client.style.fonts import getFont
 from client.style.constants import EMERAUDE, BLACK, GRAY, BLEU
 from client.style.fonts import getFontSize
-<<<<<<< HEAD
 import webbrowser
-
-=======
 from client.lib.utils import getMaps
->>>>>>> 1e017e9d85366eb15ee6ad12f86ee68494b443d2
+
 
 class HomeScreen(Screen):
     def __init__(self, window):
@@ -30,7 +27,6 @@ class HomeScreen(Screen):
         self.window.blit(label_surface, (rect.x, rect.y))
     
     def UpdateView(self):
-<<<<<<< HEAD
         title_rect = pygame.Rect((self.window.get_width() - (self.window.get_width()-180-500))//2, self.window.get_height()*0.2, 500, 100)
         pygame.draw.rect(self.window, (0,0,0), title_rect)
         self.render_label("Bienvenue sur Elyon Tanks", title_rect, 60)
@@ -47,71 +43,3 @@ class HomeScreen(Screen):
             if type == pygame.MOUSEBUTTONDOWN:
                 webbrowser.open("https://elyon.younity-mc.fr/documentation")
         self.navbar.HandleEvent(type, event)
-=======
-        self.surface.blit(self.navbar.render(), (0, 0))
-        title_rect = pygame.Rect(self.window.get_width()/2 - 250, self.window.get_height()*0.2, 300, 100)
-        self.render_label("Bienvenue sur Elyon Tanks", title_rect, 60)
-        pygame.draw.rect(self.surface, (0, 128, 255), self.create_button)
-        font = pygame.font.Font(None, 36)
-        text = font.render('Créer une partie', True, (255, 255, 255))
-        self.surface.blit(text, (self.create_button.x + 10, self.create_button.y + 10))
-
-        if self.show_modal:
-            self.draw_modal()
-
-    def HandleEvent(self, type, event):
-        self.navbar.HandleEvent(type, event)
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if self.create_button.collidepoint(event.pos):
-                self.show_modal = True
-            elif self.show_modal:
-                modal_rect = pygame.Rect(100, 100, 400, 300)
-                private_checkbox = pygame.Rect(modal_rect.x + 10, modal_rect.y + 50, 20, 20)
-                if private_checkbox.collidepoint(event.pos):
-                    self.is_private = not self.is_private
-
-                map_selector = pygame.Rect(modal_rect.x + 10, modal_rect.y + 100, 380, 30)
-                if map_selector.collidepoint(event.pos):
-                    self.dropdown_open = not self.dropdown_open
-                elif self.dropdown_open:
-                    for map_button, map_data in self.map_buttons:
-                        if map_button.collidepoint(event.pos):
-                            self.selected_map = map_data
-                            self.dropdown_open = False
-                            break
-                else:
-                    self.dropdown_open = False
-
-    def draw_modal(self):
-        modal_rect = pygame.Rect(100, 100, 400, 300)
-        pygame.draw.rect(self.surface, (200, 200, 200), modal_rect)
-        font = pygame.font.Font(None, 36)
-        text = font.render('Créer une partie', True, (0, 0, 0))
-        self.surface.blit(text, (modal_rect.x + 10, modal_rect.y + 10))
-
-        private_checkbox = pygame.Rect(modal_rect.x + 10, modal_rect.y + 50, 20, 20)
-        pygame.draw.rect(self.surface, (0, 0, 0), private_checkbox, 2)
-        if self.is_private:
-            pygame.draw.rect(self.surface, (0, 0, 0), private_checkbox)
-
-        text = font.render('Partie privée', True, (0, 0, 0))
-        self.surface.blit(text, (private_checkbox.x + 30, private_checkbox.y))
-
-        map_selector = pygame.Rect(modal_rect.x + 10, modal_rect.y + 100, 380, 30)
-        pygame.draw.rect(self.surface, (0, 0, 0), map_selector, 2)
-        if self.selected_map:
-            text = font.render(self.selected_map["name"], True, (0, 0, 0))
-            self.surface.blit(text, (map_selector.x + 10, map_selector.y + 5))
-        else:
-            text = font.render('Select a map', True, (0, 0, 0))
-            self.surface.blit(text, (map_selector.x + 10, map_selector.y + 5))
-
-        if self.dropdown_open:
-            self.map_buttons = []
-            for i, map_data in enumerate(self.maps):
-                map_button = pygame.Rect(modal_rect.x + 10, modal_rect.y + 140 + i * 40, 380, 30)
-                pygame.draw.rect(self.surface, (0, 0, 0), map_button, 2)
-                text = font.render(map_data["name"], True, (0, 0, 0))
-                self.surface.blit(text, (map_button.x + 10, map_button.y + 5))
-                self.map_buttons.append((map_button, map_data))
->>>>>>> 1e017e9d85366eb15ee6ad12f86ee68494b443d2
