9d08dad0b29245332ba0f4b5fbafb8fae62aa4b7
RGreenWolf <57684328+RGreenWolf@users.noreply.github.com>
2025-03-23 21:49:34 +0100

    patch middleware auth

--- src/server/middleware/auth.py
+++ src/server/middleware/auth.py
@@ -1,10 +1,7 @@
 from functools import wraps
-from flask import request, jsonify, session
+from flask import request, jsonify
 from server.services.tokens import verify_jwt_token
 from typing import Callable, Any
-import time
-
-cache = {}
 
 def login_required(f: Callable[..., Any]) -> Callable[..., Any]:
     @wraps(f)
@@ -12,29 +9,13 @@ def login_required(f: Callable[..., Any]) -> Callable[..., Any]:
         auth_header = request.headers.get("Authorization")
         if not auth_header:
             return jsonify({"error": "Authentification requise"}), 401
-        
-        token = auth_header.split(" ")[1]
-        current_time = time.time()
-        
-        if token in cache:
-            user_data, timestamp = cache[token]
-            if current_time - timestamp < 300:
-                request.user_id = user_data.get("user_id")
-                request.user_data = user_data
-                session["user_id"] = user_data.get("user_id")
-                return f(*args, **kwargs)
-            else:
-                del cache[token]
-        
         try:
+            token = auth_header.split(" ")[1]
             user_data = verify_jwt_token(token)
             user_id = user_data.get("user_id")
             request.user_id = user_id
             request.user_data = user_data
-            session["user_id"] = user_id
-            cache[token] = (user_data, current_time)
         except Exception as e:
             return jsonify({"error": str(e)}), 401
-        
         return f(*args, **kwargs)
     return decorated_function
